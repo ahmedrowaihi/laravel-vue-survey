8cfddf3d49f43f7caa2eac2a3d52af4f82f03147
Ahmed Rowaihi <67356781+ahmedrowaihi@users.noreply.github.com>
2022-06-01 13:41:20 +0300

    added cors and credintials

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -4,7 +4,7 @@ import store from "./store";
 const axiosClient = axios.create({
   baseURL: "http://localhost:8000/api/",
 });
-
+axiosClient.defaults.withCredentials = true;
 axiosClient.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${store.state.user.token}`;
   return config;
